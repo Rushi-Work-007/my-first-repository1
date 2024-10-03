d1f249fb9aa8172fed02cc0eb0d68cc96e4e0242
“Rushi-007-wwork” <rushikeshjondhale@gmail.com>
2024-10-03 15:20:27 +0530

    Update UserService.java

--- User-Management-Webapp/src/main/java/com/example/usermanagement/service/UserService.java
+++ User-Management-Webapp/src/main/java/com/example/usermanagement/service/UserService.java
@@ -88,7 +88,7 @@ public class UserService {
 	}
 
 	// Delete user by ID with existence check
-	public void deleteById(Long userId) {
+	public void deleteById22(Long userId) {
 		logger.info("Attempting to delete user by ID: {}", userId);
 		if (userRepository.existsById(userId)) {
 			userRepository.deleteById(userId);
